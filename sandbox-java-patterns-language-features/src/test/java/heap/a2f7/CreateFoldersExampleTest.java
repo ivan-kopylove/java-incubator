dd58fc511ae198e0ea845daa91b957f977b04e99
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-08-27 10:26:41 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/heap/a2f7/CreateFoldersExampleTest.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/a2f7/CreateFoldersExampleTest.java
@@ -13,32 +13,24 @@ import static org.junit.jupiter.api.Assertions.fail;
 /**
  * java.io.* playground
  */
-class CreateFoldersExampleTest
-{
+class CreateFoldersExampleTest {
     @Test
-    void main()
-    {
+    void main() {
         String property = System.getProperty("java.io.tmpdir");
 
         String path1 = property + "\\some_dir/subdir1\\123";
         String path2 = property + "\\some_dir//subdir2\\123";
         String path3 = property + "\\some_dir/subdir3\\\\123";
 
-        try
-        {
+        try {
             Path p1 = Files.createDirectories(Paths.get(path1));
             Path p2 = Files.createDirectories(Paths.get(path2));
             Path p3 = Files.createDirectories(Paths.get(path3));
 
-            assertTrue(p1.toFile()
-                         .exists());
-            assertTrue(p2.toFile()
-                         .exists());
-            assertTrue(p3.toFile()
-                         .exists());
-        }
-        catch (IOException e)
-        {
+            assertTrue(p1.toFile().exists());
+            assertTrue(p2.toFile().exists());
+            assertTrue(p3.toFile().exists());
+        } catch (IOException e) {
             fail();
             e.printStackTrace();
         }
