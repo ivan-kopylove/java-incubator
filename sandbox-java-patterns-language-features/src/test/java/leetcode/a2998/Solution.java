f63dd4e28fe4cc48e3d42be4f15a9459ec3f366a
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-08-29 19:37:22 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/leetcode/a2998/Solution.java
+++ sandbox-java-patterns-language-features/src/test/java/leetcode/a2998/Solution.java
@@ -4,10 +4,10 @@ class Solution
 {
     public int minimumOperationsToMakeEqual(int x, int y)
     {
-        return dfs(x, y, 0);
+        return dfs(x, y, 0, new int[x + 200]);
     }
 
-    int dfs(int x, int y, int shift)
+    int dfs(int x, int y, int shift, int[] memo)
     {
         if (x == y)
         {
@@ -19,6 +19,11 @@ class Solution
             return y - x;
         }
 
+        if(memo[x] != 0)
+        {
+            return memo[x];
+        }
+
         int call1 = Integer.MAX_VALUE - 50000;
         int call2 = Integer.MAX_VALUE - 50000;
         int call3 = Integer.MAX_VALUE - 50000;
@@ -28,30 +33,31 @@ class Solution
 
         if (x % 11 == 0)
         {
-            call1 = dfs(x / 11, y, 0);
+            call1 = dfs(x / 11, y, 0, memo);
         }
 
         if (x % 5 == 0)
         {
-            call2 = dfs(x / 5, y, 0);
+            call2 = dfs(x / 5, y, 0, memo);
         }
 
         if (shift == 0)
         {
-            call3 = dfs(x - 1, y, shift - 1);
-            call4 = dfs(x + 1, y, shift + 1);
+            call3 = dfs(x - 1, y, shift - 1, memo);
+            call4 = dfs(x + 1, y, shift + 1, memo);
 
         }
         else if(shift > 0 && shift < 7)
         {
-            call5 = dfs(x + 1, y, shift + 1);
+            call5 = dfs(x + 1, y, shift + 1, memo);
         }
-        else if (shift < 0 && shift > -200)
+        else if (shift < 0 && shift > -10000)
         {
-            call6 = dfs(x - 1, y, shift - 1);
+            call6 = dfs(x - 1, y, shift - 1, memo);
         }
 
 
-        return 1 + Math.min(call1, Math.min(call2, Math.min(call3, Math.min(call4, Math.min(call5, call6)))));
+        memo[x] = 1 + Math.min(call1, Math.min(call2, Math.min(call3, Math.min(call4, Math.min(call5, call6)))));
+        return memo[x];
     }
 }
