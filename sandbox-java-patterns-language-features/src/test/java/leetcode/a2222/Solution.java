4bec6e6e25d4748d2b427bbaeeb72f83f359e82b
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-01 11:09:53 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/leetcode/a2222/Solution.java
+++ sandbox-java-patterns-language-features/src/test/java/leetcode/a2222/Solution.java
@@ -4,6 +4,7 @@ class Solution
 {
     public long numberOfWays(String s)
     {
+
         return dfs(new StringBuilder(), 0, s);
     }
 
@@ -14,36 +15,37 @@ class Solution
             return 1;
         }
 
-        if(i >= s.length())
+        if (i >= s.length())
         {
             return 0;
         }
 
-
         int counter = 0;
+        char currentChar = s.charAt(i);
         if (current.length() == 0)
         {
-            current.append(s.charAt(i));
+            current.append(currentChar);
             counter += dfs(current, i + 1, s);
             current.deleteCharAt(current.length() - 1);
         }
         else if (current.length() > 0)
         {
-            if (current.charAt(current.length() - 1) == '1' && s.charAt(i) == '0')
+            if (current.charAt(current.length() - 1) == '1' && currentChar == '0')
             {
-                current.append(s.charAt(i));
+                current.append(currentChar);
                 counter += dfs(current, i + 1, s);
                 current.deleteCharAt(current.length() - 1);
             }
-            else if(current.charAt(current.length() - 1) == '0' && s.charAt(i) == '1')
+            else if (current.charAt(current.length() - 1) == '0' && currentChar == '1')
             {
-                current.append(s.charAt(i));
+                current.append(currentChar);
                 counter += dfs(current, i + 1, s);
                 current.deleteCharAt(current.length() - 1);
             }
         }
 
         counter += dfs(current, i + 1, s);
+
         return counter;
     }
 }
