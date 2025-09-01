4b4656edb8c9e3d57e923b0a471f24e2616edd63
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-01 12:54:48 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/leetcode/a11/Solution.java
+++ sandbox-java-patterns-language-features/src/test/java/leetcode/a11/Solution.java
@@ -2,8 +2,8 @@ package leetcode.a11;
 
 /**
  * @see <a href="https://ivan-kopylove.github.io/leetcode/6b0b">garden</a>
- * @see <a href="https://leetcode.com/problems/container-with-most-water/submissions/449358581/">submission</a>
  * @see <a href="https://github.com/search?q=user%3Aivan-kopylove+11+container">github</a>
+ * @see <a href="https://leetcode.com/problems/container-with-most-water/submissions/449358581/">submission</a>
  * @see <a href="obsidian://search?query=leetcode 11 container">obsidian</a>
  */
 class Solution
