6101f357010080f8cfc15ab1a2188a3b4e210f0d
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-01 12:49:32 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/leetcode/a2571/ee94.java
+++ sandbox-java-patterns-language-features/src/test/java/leetcode/a2571/ee94.java
@@ -4,13 +4,10 @@ import org.junit.jupiter.api.Test;
 
 class ee94
 {
-
-    /**
-     * what will be printed?
-     * what kind of number 32 is?
-     * what if there were another number?
-     * what does & operator do?
-     */
+    /// - what will be printed?
+    /// - what kind of number 32 is?
+    /// - what if there were another number?
+    /// - what does & operator do?
     @Test
     void what_is_the_point_of_this()
     {
