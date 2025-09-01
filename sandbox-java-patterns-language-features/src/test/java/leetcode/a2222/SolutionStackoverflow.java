8eb548afe9325e6e7fc77bc68af95211b5003e12
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-01 12:55:17 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/leetcode/a2222/SolutionStackoverflow.java
+++ sandbox-java-patterns-language-features/src/test/java/leetcode/a2222/SolutionStackoverflow.java
@@ -1,9 +1,9 @@
 package leetcode.a2222;
 
 /**
- * @see <a href="https://leetcode.com/problems/number-of-ways-to-select-buildings/submissions/1755549166/">submission</a>
  * @see <a href="https://ivan-kopylove.github.io/leetcode/8b49">garden</a>
  * @see <a href="https://github.com/search?q=user%3Aivan-kopylove+2222">github</a>
+ * @see <a href="https://leetcode.com/problems/number-of-ways-to-select-buildings/submissions/1755549166/">submission</a>
  * @see <a href="obsidian://search?query=leetcode 2222">obsidian</a>
  */
 class SolutionStackoverflow
