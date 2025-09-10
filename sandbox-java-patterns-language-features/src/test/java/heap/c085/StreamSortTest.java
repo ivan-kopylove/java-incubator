191579f2f1135e37a701890fab1f4e29317da585
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-10 13:23:09 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/heap/c085/StreamSortTest.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/c085/StreamSortTest.java
@@ -2,7 +2,10 @@ package heap.c085;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 class StreamSortTest
@@ -11,7 +14,7 @@ class StreamSortTest
     @Test
     void nullSafeChainedLambdaComparator()
     {
-        List<ComparableObject> list = DataProvider.createObjectsWithNulls();
+        List<ComparableObject> list = createObjectsWithNulls();
         list.sort(Comparator.nullsLast(Comparator.comparing(ComparableObject::getDate))
                             .thenComparing(Comparator.nullsLast(Comparator.comparing(ComparableObject::getStr))));
     }
@@ -20,7 +23,25 @@ class StreamSortTest
     @Test
     void chainedLambdaComparator()
     {
-        List<ComparableObject> list = DataProvider.createObjects();
+        List<ComparableObject> list = createObjects();
         list.sort(Comparator.comparing(ComparableObject::getDate).thenComparing(ComparableObject::getStr));
+    }
+
+    private List<ComparableObject> createObjects()
+    {
+        return new ArrayList<>(Arrays.asList(new ComparableObject("str1", new Date(), 4),
+                                             new ComparableObject("str2", new Date(55555555), 3),
+                                             new ComparableObject("str3", new Date(55555555), 6)
+
+        ));
+    }
+
+    private List<ComparableObject> createObjectsWithNulls()
+    {
+        return new ArrayList<>(Arrays.asList(null,
+                                             new ComparableObject("str1", new Date(), 4),
+                                             new ComparableObject("str2", new Date(55555555), 3),
+                                             new ComparableObject("str3", new Date(55555555), 6),
+                                             null));
     }
 }
