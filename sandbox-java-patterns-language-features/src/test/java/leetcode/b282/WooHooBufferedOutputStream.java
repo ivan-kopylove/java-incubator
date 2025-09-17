31d57bb920b58c9a77d69c2064c75d0157ba8acc
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-17 11:57:01 +0300

    wip

---
+++ sandbox-java-patterns-language-features/src/test/java/leetcode/b282/WooHooBufferedOutputStream.java
@@ -0,0 +1,50 @@
+package leetcode.b282;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WooHooBufferedOutputStream
+{
+    private static int                  n   = 0;
+    private static BufferedOutputStream out = new BufferedOutputStream(System.out, 64 * 1024);
+    private static byte[]               chars;
+
+    public static void main(String[] args) throws Exception
+    {
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = r.readLine();
+        n = Integer.parseInt(line);
+
+        chars = new byte[n * 2 + 1]; // what does plus one give?
+        chars[chars.length - 1] = '\n';
+
+        dfs(0, 0, n);
+        out.flush();
+    }
+
+    private static void dfs(int left, int right, int n) throws IOException
+    {
+        if (left == n && right == n)
+        {
+            out.write(chars);
+            return;
+        }
+
+        if (left < n)
+        {
+            chars[left + right] = '(';
+            dfs(left + 1, right, n);
+        }
+
+        if (right < left)
+        {
+            chars[left + right] = ')';
+            dfs(left, right + 1, n);
+        }
+    }
+}
+
