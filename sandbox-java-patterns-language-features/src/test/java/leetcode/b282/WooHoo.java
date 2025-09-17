c8cc09fcad3dd89675fcdd42795e137543029bb8
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-17 10:33:49 +0300

    wip

---
+++ sandbox-java-patterns-language-features/src/test/java/leetcode/b282/WooHoo.java
@@ -0,0 +1,43 @@
+package leetcode.b282;
+
+import java.io.*;
+
+public class WooHoo
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = r.readLine();
+    
+    	int n = Integer.parseInt(line);
+        dfs(new StringBuilder(6), 0, 0, n);
+    }
+
+	private static void dfs(StringBuilder current, int left, int right, int n)
+    {
+    	if(current.length() == n * 2)
+        {
+            System.out.println(current);
+        	return;
+        }
+    
+    	if(left < n)
+        {
+            current.append("(");
+        	dfs(current, left + 1, right, n);
+            current.deleteCharAt(current.length() - 1);
+        }
+    
+    	if(right < left)
+        {
+            current.append(")");
+        	dfs(current, left, right + 1, n);
+            current.deleteCharAt(current.length() - 1);
+        }
+    	
+    	
+    	
+    }
+}
+
