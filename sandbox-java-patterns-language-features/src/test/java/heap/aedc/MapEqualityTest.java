94a361dd30b888c2d2b714cafe535a1b5b39eedf
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-17 12:18:08 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/heap/aedc/MapEqualityTest.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/aedc/MapEqualityTest.java
@@ -24,7 +24,7 @@ class MapEqualityTest
         map2.put(2, 4);
 
 
-        System.out.println(map1.equals(map2)); // how can I get an evidence prooving or falsifying this?
+        System.out.println(map1.equals(map2)); // how can I get an evidence prooving or falsifying this (without running)?
         fail();
         assertEquals(map1, map2);
     }
