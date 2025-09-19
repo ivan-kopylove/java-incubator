8fdc4b8b8920365ecd75ec19071240a730a9288b
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-19 11:29:31 +0300

    wip

---
+++ sandbox-hibernate5/src/test/java/kopylove/hibernate/heap/a03/ChildEntity14.java
@@ -0,0 +1,52 @@
+package kopylove.hibernate.heap.a03;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+class ChildEntity14
+{
+    @Id
+    @Column(name = "CHILDTABLE_KEY", unique = true, nullable = false)
+    private Integer key;
+
+    @Column(name = "CHILDTABLE_NAME")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "CHILDTABLE_PARENT_KEY", nullable = false)
+    private kopylove.hibernate.ParentEntity parent;
+
+    public Integer getKey()
+    {
+        return key;
+    }
+
+    public void setKey(Integer key)
+    {
+        this.key = key;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public kopylove.hibernate.ParentEntity getParent()
+    {
+        return parent;
+    }
+
+    public void setParent(kopylove.hibernate.ParentEntity parent)
+    {
+        this.parent = parent;
+    }
+}
