1bb598ba4b37b878e520278b0424a7519046b90b
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-19 12:43:51 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/heap/asddas/EducationalStrengthness.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/asddas/EducationalStrengthness.java
@@ -3,7 +3,7 @@ package heap.asddas;
 public enum EducationalStrengthness
 {
 
-    KOAN, // some tests will fail
+    KOAN, // some tests will fail on purpose
     EXAMPLE;
 
     public static EducationalStrengthness EDUCATION_MODE = EXAMPLE;
