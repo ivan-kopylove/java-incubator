616d6e66a1f47298f24e242d2a5ad57ca2fa211c
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-19 13:32:33 +0300

    p

--- sandbox-java-patterns-language-features/src/test/java/heap/a976/NullValueExamplesTest.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/a976/NullValueExamplesTest.java
@@ -2,14 +2,20 @@ package heap.a976;
 
 import org.junit.jupiter.api.Test;
 
+import static heap.cb2d.EducationalStrengthness.EDUCATION_MODE;
+import static heap.cb2d.EducationalStrengthness.KOAN;
+import static org.junit.jupiter.api.Assertions.fail;
+
 class NullValueExamplesTest
 {
     @Test
     void what_will_be_printed()
     {
-        if (false) // try to predict yourself before switching
+        if (EDUCATION_MODE == KOAN)
         {
-            System.out.println(null == null);
+            fail("try to predict yourself before running the assertion");
         }
+
+        System.out.println(null == null);
     }
 }
