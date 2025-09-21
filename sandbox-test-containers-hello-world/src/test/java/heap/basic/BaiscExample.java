95683278a6f185c40224575397a0336935a75192
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-21 10:57:45 +0300

    wip

--- sandbox-test-containers-hello-world/src/test/java/heap/basic/BaiscExample.java
+++ sandbox-test-containers-hello-world/src/test/java/heap/basic/BaiscExample.java
@@ -1,5 +1,7 @@
 package heap.basic;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 
@@ -8,17 +10,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BaiscExample
 {
+    private static GenericContainer container;
+
+    @BeforeAll
+    public static void populate()
+    {
+        container = new GenericContainer("alpine:3.2").withExposedPorts(80);
+    }
+
+    @AfterAll
+    public static void destroy()
+    {
+        container.stop();
+    }
+
     @Test
     void what_will_be_printed()
     {
-        // given
-        GenericContainer container = new GenericContainer("alpine:3.2").withExposedPorts(80);
-
         // when
         container.start();
 
         // then
-
         String address = container.getHost() + ":" + container.getExposedPorts().getFirst();
         System.out.println(address);
         assertThat(address, equalTo("localhost:80"));
