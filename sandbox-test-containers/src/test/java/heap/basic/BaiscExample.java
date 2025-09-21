8c377ab0415f068d4323aa5f2159f8297b410b8b
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-21 10:18:53 +0300

    wip

--- sandbox-test-containers/src/test/java/heap/basic/BaiscExample.java
+++ sandbox-test-containers/src/test/java/heap/basic/BaiscExample.java
@@ -3,22 +3,23 @@ package heap.basic;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class BaiscExample
 {
     @Test
     void what_will_be_printed()
     {
         // given
-        GenericContainer simpleWebServer = new GenericContainer("alpine:3.2")
-                .withExposedPorts(80)
-                                                                             .withCommand("/bin/sh",
-                                                                                          "-c",
-                                                                                          "while true; do echo " + "\"HTTP/1.1 200 OK\n\nHello World!\" | nc -l -p 80; done");
+        GenericContainer container = new GenericContainer("alpine:3.2").withExposedPorts(80);
 
-        simpleWebServer.start();
+        // when
+        container.start();
 
-
-        System.out.println(simpleWebServer.getHost() + ":" + simpleWebServer.getExposedPorts().getFirst());
-
+        // then
+        String address = container.getHost() + ":" + container.getExposedPorts().getFirst();
+        System.out.println(address);
+        assertThat(address, equalTo("localhost:80"));
     }
 }
