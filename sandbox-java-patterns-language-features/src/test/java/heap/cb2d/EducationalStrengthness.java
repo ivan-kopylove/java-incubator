39722444efed931fa65180996f7bd31e3f2ee946
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-21 11:07:19 +0300

    wip

--- sandbox-java-patterns-language-features/src/test/java/heap/cb2d/EducationalStrengthness.java
+++ sandbox-java-patterns-language-features/src/test/java/heap/cb2d/EducationalStrengthness.java
@@ -3,7 +3,7 @@ package heap.cb2d;
 public enum EducationalStrengthness
 {
 
-    KOAN, // some tests will fail on purpose
+    KOAN, // interactive bumpy road - tests will fail on purpose
     EXAMPLE; // everything is smooth
 
     public static EducationalStrengthness EDUCATION_MODE = EXAMPLE;
