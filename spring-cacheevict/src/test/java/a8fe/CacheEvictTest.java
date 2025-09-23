2058c4dd134fae2f01a8a483044d8c57bf1a487f
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-23 18:13:03 +0300

    wip

--- spring-cacheevict/src/test/java/a8fe/CacheEvictTest.java
+++ spring-cacheevict/src/test/java/a8fe/CacheEvictTest.java
@@ -16,11 +16,54 @@ public class CacheEvictTest
 
         SomeService3 service = context.getBean(SomeService3.class);
 
-        System.out.println(service.generateUuid(1));
-        System.out.println(service.generateUuid(1));
-        System.out.println(service.generateUuid2(1));
-
-        assertThat(service.generateUuid(1), equalTo(service.generateUuid2(1)));
-        assertThat(service.generateUuid(1), equalTo(service.generateUuid2(1)));
+        System.out.println(service.associateMyArg(1));
+        System.out.println(service.associateMyArg(1));
+        service.evictMyCache(2);
+        System.out.println(service.associateMyArg(1));
     }
+
+
+    @Test
+    void what_will_be_printed2()
+    {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig3.class);
+
+        SomeService3 service = context.getBean(SomeService3.class);
+
+        System.out.println(service.associateMyArg(3));
+        System.out.println(service.associateMyArg(3));
+        service.evictMyCache(3);
+        System.out.println(service.associateMyArg(3));
+    }
+
+
+
+    @Test
+    void what_will_be_printed3()
+    {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig3.class);
+
+        SomeService3 service = context.getBean(SomeService3.class);
+
+        System.out.println(service.associateMyArg(3));
+        System.out.println(service.associateMyArg(3));
+        service.evictMyCache();
+        System.out.println(service.associateMyArg(3));
+    }
+
+
+    @Test
+    void what_will_be_printed4()
+    {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig3.class);
+
+        SomeService3 service = context.getBean(SomeService3.class);
+
+        System.out.println(service.associateMyArg(3));
+        System.out.println(service.associateMyArg(3));
+        service.evictAllEntries();
+        System.out.println(service.associateMyArg(3));
+    }
+
+
 }
--- spring-cacheevict/src/test/java/a8fe/SomeService3.java
+++ spring-cacheevict/src/test/java/a8fe/SomeService3.java
@@ -1,20 +1,29 @@
 // com.example.SomeService3.java
 package a8fe;
 
-import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 
 import static java.util.UUID.randomUUID;
 
 public class SomeService3
 {
-    @CachePut("myCache3")
-    public String generateUuid(int someInt) {
-        return randomUUID().toString();
+    @CacheEvict("myCache3")
+    public void evictMyCache(int someInt) {
     }
 
+
+    @CacheEvict("myCache3")
+    public void evictMyCache() {
+    }
+    @CacheEvict(value = "myCache3", allEntries = true)
+    public void evictAllEntries() {
+    }
+
+
+
     @Cacheable("myCache3")
-    public String generateUuid2(int someInt) {
+    public String associateMyArg(int someInt) {
         return randomUUID().toString();
     }
 
