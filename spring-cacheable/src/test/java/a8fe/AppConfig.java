092ef321d3a790a5b75824b45f780d3dd01a2a4c
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-23 17:39:15 +0300

    wip

--- spring-cacheable/src/test/java/a8fe/AppConfig.java
+++ spring-cacheable/src/test/java/a8fe/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableCaching // why Srping doesn't throw an exception for method marked @Cacheable when cache is not enabled?
+@EnableCaching // why Spring doesn't throw an exception for method marked @Cacheable when cache is not enabled?
 @Configuration
 @ComponentScan(basePackages = {"a8fe"})
 public class AppConfig
