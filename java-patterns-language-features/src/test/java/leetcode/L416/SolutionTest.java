c78a3f28fa643ac54031db480ca18beb0e154bb9
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-27 15:48:24 +0300

    LEETCODE-279

--- java-patterns-language-features/src/test/java/leetcode/L416/SolutionTest.java
+++ java-patterns-language-features/src/test/java/leetcode/L416/SolutionTest.java
@@ -1,17 +1,40 @@
 package leetcode.L416;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 
+import static heap.cb2d.EducationalStrengthness.EDUCATION_MODE;
+import static heap.cb2d.EducationalStrengthness.KOAN;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SolutionTest
 {
+
+    @Test
+    void fixMissingSantiyCheck()
+    {
+        if (EDUCATION_MODE != KOAN)
+        {
+            return;
+        }
+
+        // given
+        FixMissingSantiyCheck sut = new FixMissingSantiyCheck();
+
+        // when
+        boolean result = sut.canPartition(new int[]{1, 5, 3});
+
+        // then
+        assertThat(result, equalTo(result));
+    }
+
     @ParameterizedTest
     @MethodSource("dataSet")
     void foo(int[] input, boolean expected)
