ebe8680eb50a246a730b08c98e9d7dcab606d60d
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-28 10:41:22 +0300

    FISH-0005 refactored

--- java-patterns-language-features/src/test/java/heap/a4cd/EvaluationOrderTest.java
+++ java-patterns-language-features/src/test/java/heap/a4cd/EvaluationOrderTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class EvaluationOrderTest
 {
     @Test
-    void calculateYourselfFirst1()
+    void exercise_design_reverse_engineering1()
     {
         int x = 5;
         int y = 10;
@@ -15,7 +15,7 @@ class EvaluationOrderTest
     }
 
     @Test
-    void calculateYourselfFirst2()
+    void exercise_design_reverse_engineering2()
     {
         String result1 = "1 + 2 = " + 1 + 2;
         String result2 = "1 + 2 = " + (1 + 2);
@@ -26,7 +26,7 @@ class EvaluationOrderTest
     }
 
     @Test
-    void calculateYourselfFirst4()
+    void exercise_design_reverse_engineering4()
     {
         String result1 = 1 + 2 + "abc";
         String result2 = "abc" + 1 + 2;
@@ -37,7 +37,7 @@ class EvaluationOrderTest
     }
 
     @Test
-    void calculateYourselfFirst3()
+    void exercise_design_reverse_engineering3()
     {
         int year = 2020;
         boolean result = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
