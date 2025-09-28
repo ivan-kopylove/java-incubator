3ef59f1f5c1a17d14bbd8cb9325c93f6fd7421fa
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-28 10:39:55 +0300

    FISH-0005 refactored

--- java-patterns-language-features/src/test/java/heap/a27f/Runner.java
+++ java-patterns-language-features/src/test/java/heap/a27f/Runner.java
@@ -2,20 +2,12 @@ package heap.a27f;
 
 import org.junit.jupiter.api.Test;
 
-/// java.mechanics.exceptions.nestedclasses
 class Runner
 {
-    @Test
-    void haveToWrapWithTry()
-    {
-        Parent sut = new Child();
-        sut.run();
-    }
-
     @Test
     void dontHaveToWrapWithTry()
     {
         Child sut = new Child();
-        sut.run();
+        sut.myMethod();
     }
 }
