099ba63ba60c882fd80fa4f219253c464614c189
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-28 08:21:13 +0300

    LEETCODE-279

--- java-patterns-language-features/src/test/java/leetcode/L416/FixMissingSanityCheckKoan.java
+++ java-patterns-language-features/src/test/java/leetcode/L416/FixMissingSanityCheckKoan.java
@@ -1,6 +1,5 @@
 package leetcode.L416;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,29 +13,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-class SolutionTest
+class FixMissingSanityCheckKoan
 {
 
-    @ParameterizedTest
-    @MethodSource("dataSet")
-    void foo(int[] input, boolean expected)
+    @Test
+    void fixMissingSantiyCheck()
     {
+        if (EDUCATION_MODE != KOAN)
+        {
+            return;
+        }
+
         // given
         FixMissingSantiyCheck sut = new FixMissingSantiyCheck();
 
         // when
-        boolean result = sut.canPartition(input);
+        boolean result = sut.canPartition(new int[]{1, 5, 3});
 
         // then
-        assertThat(result, equalTo(expected));
-    }
-
-    private static List<Arguments> dataSet()
-    {
-        // @formatter:off
-        return List.of(
-                arguments(new int[]{1, 5, 3}, false)
-        );
-        // @formatter:on
+        assertThat(result, equalTo(result));
     }
 }
