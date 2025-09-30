88a5e95f96f2e36edc0f6a70870bf178a0096a9e
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-30 16:36:56 +0300

    reorganized modules

---
+++ java-concurrency/src/test/java/named/concurrencyLiveLock/LiveLockTest.java
@@ -0,0 +1,85 @@
+package named.concurrencyLiveLock;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.github.ivan.kopylove.commons.testing.TestBase;
+class LiveLockTest extends TestBase
+{
+    private static void sleep(int millis)
+    {
+        try
+        {
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void what_is_the_purpose_of_this_example()
+    {
+        // given
+        ReentrantLock lock1 = new ReentrantLock(true);
+        ReentrantLock lock2 = new ReentrantLock(true);
+
+        new Thread(() -> {
+            while (true)
+            {
+                lock1.tryLock();
+
+                sleep(5_000);
+
+                if (lock2.tryLock())
+                {
+                    System.out.println("thread 1 - lock 2 locked");
+                }
+                else
+                {
+                    System.out.println("thread 1 - lock 2 cannot be locked, unlocking lock 1");
+                    lock1.unlock();
+                    continue;
+                }
+
+                System.out.println("thread 1 - reached end");
+                break;
+            }
+
+            lock1.unlock();
+            lock2.unlock();
+        }).start();
+
+
+        new Thread(() -> {
+            while (true)
+            {
+                lock2.tryLock();
+
+                sleep(5_000);
+
+                if (lock1.tryLock())
+                {
+                    System.out.println("thread 2 - lock 1 locked");
+                }
+                else
+                {
+                    System.out.println("thread 2 - lock 1 cannot be locked, unlocking lock 1");
+                    lock2.unlock();
+                    continue;
+                }
+
+                System.out.println("thread 2 - reached end.");
+                break;
+            }
+
+            lock2.unlock();
+            lock1.unlock();
+        }).start();
+
+        // when
+        sleep(15_000);
+    }
+}
---
+++ java-concurrency/src/test/java/named/concurrencyWaiterError/WaitErrorExampleTest.java
@@ -0,0 +1,53 @@
+package named.concurrencyWaiterError;
+
+// Example 1: Basic Illegal Monitor State (Run to see error)
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.github.ivan.kopylove.commons.testing.TestBase;
+class WaitErrorExampleTest extends TestBase
+{
+    @Nested
+    class Problem
+    {
+        @Test
+        void should_throw_because_monitor_is_not_locked()
+        {
+            // given
+            Object lock = new Object();
+
+            // when
+            IllegalMonitorStateException ex = assertThrows(IllegalMonitorStateException.class, () -> {
+                lock.wait(); // ← why will throw exception?
+            });
+
+
+            // then
+            assertThat(ex.getMessage(), equalTo("current thread is not owner")); // (is not an owner of what?);
+        }
+    }
+
+    @Nested
+    class Solution
+    {
+        private final Object lock = new Object();
+
+        @Test
+        void should_call_wait_normally() throws InterruptedException
+        {
+            // given
+            synchronized (lock)
+            {
+                System.out.println("Entered sync block");
+                // when
+                lock.wait(5000); // how this is supposed to be used without timeouts?
+                System.out.println("Will this be printed somewhen?");
+            }
+        }
+    }
+}
