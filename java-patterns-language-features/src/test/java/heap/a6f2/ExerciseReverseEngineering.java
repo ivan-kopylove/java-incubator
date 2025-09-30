2026876c8295cbf1ab5b2dd6843ae38d9bfdfabd
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-09-30 12:23:47 +0300

    Dev (#15)

---
+++ java-patterns-language-features/src/test/java/heap/a6f2/ExerciseReverseEngineering.java
@@ -0,0 +1,35 @@
+package heap.a6f2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExerciseReverseEngineering
+{
+    @Test
+    void exercise_design_reverse_engineering2()
+    {
+        // given
+        int[] a = new int[] { 1, 2 };
+
+        // then
+        Map<int[], String> map = new HashMap<>();
+        map.put(a, "something");
+
+        System.out.println(map.get(a));
+        System.out.println(map.get(new int[] { 1, 2 }));
+    }
+
+    @Test
+    void exercise_design_reverse_engineering1()
+    {
+        // given
+        int[] a = new int[] { 1, 2 };
+        int[] b = new int[] { 1, 2 };
+
+        // then
+        System.out.println(a.equals(b));
+        System.out.println(a.hashCode() == b.hashCode());
+    }
+}
