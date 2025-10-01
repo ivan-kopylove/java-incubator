03d198615b35c19afc90b306cb2a57a95fe94687
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-01 17:52:57 +0300

    wip

---
+++ data-structures-algorithms-leetcode/src/test/java/leetcode/L136/package-info.java
@@ -0,0 +1,4 @@
+// package-info.java
+///
+/// this guy from ICQ with 3-letter name. Last time I used this to solve the task
+package leetcode.L136;
---
+++ data-structures-algorithms-leetcode/src/test/java/myXor/weweq.java
@@ -0,0 +1,16 @@
+package myXor;
+
+import org.junit.jupiter.api.Test;
+
+public class weweq
+{
+    @Test
+    void pronounce_property_names_out_of_loud()
+    {
+        // then
+        System.out.println(5 ^ 5);
+        System.out.println(5 ^ 0);
+        System.out.println(0 ^ 5);
+        System.out.println(((4 ^ 5) ^ 6) == ((6 ^ 5) ^ 4));
+    }
+}
