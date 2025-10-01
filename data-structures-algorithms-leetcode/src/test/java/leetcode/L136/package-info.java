1abfefb8092ec66b100bea8d150d7995afb36b20
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-01 18:09:36 +0300

    wip

--- data-structures-algorithms-leetcode/src/test/java/leetcode/L136/package-info.java
+++ data-structures-algorithms-leetcode/src/test/java/leetcode/L136/package-info.java
@@ -1,4 +1,11 @@
 // package-info.java
 ///
-/// this guy from ICQ with 3-letter name. Last time I used property of this to solve the task.
+/// Yes, it's possible to solve
+///
+/// There are a lot of pairs.
+///
+/// This guy from ICQ with 3-letter name. Last time I used property of this to collapse pairs and finding the outstanding number.
+///
+/// [leetcode.L200]
+///
 package leetcode.L136;
