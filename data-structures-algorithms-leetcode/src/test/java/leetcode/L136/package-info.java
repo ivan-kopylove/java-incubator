528c1fb54fb2c8bf0c6d33c1f87ef6b93cd1053e
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-01 17:58:15 +0300

    wip

--- data-structures-algorithms-leetcode/src/test/java/leetcode/L136/package-info.java
+++ data-structures-algorithms-leetcode/src/test/java/leetcode/L136/package-info.java
@@ -1,4 +1,4 @@
 // package-info.java
 ///
-/// this guy from ICQ with 3-letter name. Last time I used this to solve the task
+/// this guy from ICQ with 3-letter name. Last time I used property of this to solve the task.
 package leetcode.L136;
