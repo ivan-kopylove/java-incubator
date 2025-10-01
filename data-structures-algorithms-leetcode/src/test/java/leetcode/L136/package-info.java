61cc3c8c7690faa7445db7acbbe40c1cd85a0f88
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-01 18:16:08 +0300

    wip

--- data-structures-algorithms-leetcode/src/test/java/leetcode/L136/package-info.java
+++ data-structures-algorithms-leetcode/src/test/java/leetcode/L136/package-info.java
@@ -1,5 +1,3 @@
-/// package-info.java
-///
 /// There are a lot of pairs and an outstanding number.
 ///
 /// This guy from ICQ with 3-letter name. Last time I used property of this to collapse pairs and finding the outstanding number.
