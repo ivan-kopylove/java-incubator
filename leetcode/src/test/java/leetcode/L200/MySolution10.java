c11558ed166554512ef5ff4699015da1a0593fab
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-01 12:32:34 +0300

    LEETCODE-0200

---
+++ leetcode/src/test/java/leetcode/L200/MySolution10.java
@@ -0,0 +1,11 @@
+package leetcode.L200;
+
+class MySolution10
+{
+    public int numIslands(char[][] grid) {
+
+        // how do I iterate over matrices?
+
+        return 0;
+    }
+}
---
+++ leetcode/src/test/java/leetcode/L200/MySolution50.java
@@ -0,0 +1,62 @@
+package leetcode.L200;
+
+class MySolution50
+{
+    public int numIslands(char[][] grid) {
+
+        int[][] dp = new int[grid.length][grid[0].length];
+
+        int current = 0;
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                if(grid[i][j] == '1' && dp[i][j] == 0) // what if there is no such condition?
+                {
+                    current++;
+                }
+                dfs(i, j, current, grid, dp);
+            }
+        }
+
+        return current;
+    }
+
+
+    void dfs(int i, int j, int current, char[][] grid, int[][] dp)
+    {
+        // what are base cases related to very first cell 0,0 and i,j?
+
+        if(grid.length <= i)
+        {
+            return;
+        }
+
+        if(grid[i].length <= j)
+        {
+            return;
+        }
+
+        if(grid[i][j] == '0')
+        {
+            return;
+        }
+
+        if(dp[i][j] != 0)
+        {
+            return;
+        }
+
+        if(grid[i][j] == '1')
+        {
+            dp[i][j] = current;
+        }
+
+        dfs(i + 1, j, current, grid, dp);
+        dfs(i - 1, j, current, grid, dp);
+        dfs(i, j + 1, current, grid, dp);
+        dfs(i, j - 1, current, grid, dp);
+    }
+
+    
+}
