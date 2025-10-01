f75cc99deb620cb3a54d3d5c6b4ea2367521b22a
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-01 12:42:46 +0300

    LEETCODE-0200

--- leetcode/src/test/java/leetcode/L200/MySolution10.java
+++ leetcode/src/test/java/leetcode/L200/MySolution10.java
@@ -4,7 +4,7 @@ class MySolution10
 {
     public int numIslands(char[][] grid) {
 
-        // I know the task is solvable, what operations do I know?
+        // I know the task is solvable, what operations do I know so that combining them in proper order gives the result?
 
         return 0;
     }
