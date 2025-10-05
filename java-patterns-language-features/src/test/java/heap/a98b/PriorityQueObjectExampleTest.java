f10382eed6416eeb572c30504c77c158fedfed0e
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-05 12:33:12 +0300

    PriorityQueExamples

--- java-patterns-language-features/src/test/java/heap/a98b/PriorityQueObjectExampleTest.java
+++ java-patterns-language-features/src/test/java/heap/a98b/PriorityQueObjectExampleTest.java
@@ -8,29 +8,30 @@ import java.util.PriorityQueue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class ColoredNumber{
+class Foo
+{
     int id;
     String color;
 
-    public ColoredNumber(int id, String color)
+    public Foo(int id, String color)
     {
         this.id = id;
         this.color = color;
     }
 }
 
-class ColoredNumber2 implements Comparable<ColoredNumber2>{
+class Bar implements Comparable<Bar>{
     int id;
     String color;
 
-    public ColoredNumber2(int id, String color)
+    public Bar(int id, String color)
     {
         this.id = id;
         this.color = color;
     }
 
     @Override
-    public int compareTo(ColoredNumber2 o)
+    public int compareTo(Bar o)
     {
         return Integer.compare(this.id, o.id);
     }
@@ -42,11 +43,11 @@ class PriorityQueObjectExampleTest extends TestBase
     void why_this_throws()
     {
         // given
-        PriorityQueue<ColoredNumber> priorityQueue = new PriorityQueue<>();
+        PriorityQueue<Foo> priorityQueue = new PriorityQueue<>();
 
 
         assertThrows(ClassCastException.class, () -> {
-            priorityQueue.add(new ColoredNumber(5,"yellow"));
+            priorityQueue.add(new Foo(5, "yellow"));
         });
     }
 
@@ -56,11 +57,11 @@ class PriorityQueObjectExampleTest extends TestBase
     void exercise_design_reverse_engineering1()
     {
         // given
-        PriorityQueue<ColoredNumber> priorityQueue = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.id, o2.id));
+        PriorityQueue<Foo> priorityQueue = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.id, o2.id));
 
-        priorityQueue.add(new ColoredNumber(5,"yellow"));
-        priorityQueue.add(new ColoredNumber(3,"red"));
-        priorityQueue.add(new ColoredNumber(4,"green"));
+        priorityQueue.add(new Foo(5, "yellow"));
+        priorityQueue.add(new Foo(3, "red"));
+        priorityQueue.add(new Foo(4, "green"));
 
         assertEquals(priorityQueue.poll().color, "red");
         assertEquals(priorityQueue.poll().color, "green");
@@ -74,12 +75,12 @@ class PriorityQueObjectExampleTest extends TestBase
     {
         // given
 
-        PriorityQueue<ColoredNumber2> priorityQueue = new PriorityQueue<>();
+        PriorityQueue<Bar> priorityQueue = new PriorityQueue<>();
 
 
-        priorityQueue.add(new ColoredNumber2(5,"yellow"));
-        priorityQueue.add(new ColoredNumber2(3,"red"));
-        priorityQueue.add(new ColoredNumber2(4,"green"));
+        priorityQueue.add(new Bar(5, "yellow"));
+        priorityQueue.add(new Bar(3, "red"));
+        priorityQueue.add(new Bar(4, "green"));
 
         assertEquals(priorityQueue.poll().color, "red");
         assertEquals(priorityQueue.poll().color, "green");
@@ -92,12 +93,12 @@ class PriorityQueObjectExampleTest extends TestBase
     {
         // given
 
-        PriorityQueue<ColoredNumber2> priorityQueue = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.id, o1.id));
+        PriorityQueue<Bar> priorityQueue = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.id, o1.id));
 
 
-        priorityQueue.add(new ColoredNumber2(5,"yellow"));
-        priorityQueue.add(new ColoredNumber2(3,"red"));
-        priorityQueue.add(new ColoredNumber2(4,"green"));
+        priorityQueue.add(new Bar(5, "yellow"));
+        priorityQueue.add(new Bar(3, "red"));
+        priorityQueue.add(new Bar(4, "green"));
 
 
         assertEquals(priorityQueue.poll().color, "yellow");
