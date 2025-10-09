c94fbad4932cee3f5bb77a9211ac911a449cb7c5
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-09 19:48:13 +0300

    LEETCODE-0074

---
+++ data-structures-algorithms-leetcode/src/test/java/leetcode/L11/Solution30.java
@@ -0,0 +1,40 @@
+package leetcode.L11;
+
+class Solution30
+{
+    public int maxArea(int[] height)
+    {
+        int globalMax = 0;
+
+        int leftIdx = 0;
+        int rightIdx = height.length - 1;
+
+        int rightHeight = height[rightIdx];
+        int leftHeight = height[leftIdx];
+        int bottomHeight = 0;
+
+        int maxLeftHeight = leftHeight;
+        int maxRightHeight = rightHeight;
+
+        while (leftIdx != rightIdx)
+        {
+            bottomHeight = rightIdx - leftIdx;
+
+            int localMax = 0;
+            if (leftHeight <= rightHeight)
+            {
+                localMax = leftHeight * bottomHeight;
+            }
+            else
+            {
+                localMax = rightHeight * bottomHeight;
+            }
+
+            globalMax = Math.max(globalMax, localMax);
+
+            // I should move indices here as well. According what logic?
+        }
+
+        return globalMax;
+    }
+}
