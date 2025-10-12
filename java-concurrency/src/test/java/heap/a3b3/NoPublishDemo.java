bafd48dc2e00d864a3d9b53d76996220795bce17
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-10-12 19:10:27 +0300

    NoPublishDemo

---
+++ java-concurrency/src/test/java/heap/a3b3/NoPublishDemo.java
@@ -0,0 +1,37 @@
+package heap.a3b3;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/// [source](https://github.com/search?q=NoPublishDemo&type=code)
+public class NoPublishDemo
+{
+    private boolean stop = false;
+    private boolean stopped = false;
+
+    @Test
+    void exercise_design_reverse_engineering1() throws InterruptedException
+    {
+        Thread thread = new Thread(() -> {
+            System.out.println("ConcurrencyCheckTask started!");
+
+            while (!stop) {
+            }
+
+            stopped = true;
+            System.out.println("ConcurrencyCheckTask stopped");
+        });
+
+        thread.start();
+
+        Thread.sleep(1000);
+        System.out.println("Set stop to true in main");
+        stop = true;
+        System.out.println("Exit main");
+        Thread.sleep(10_000);
+        assertThat(stopped, is(false));
+    }
+}
---
+++ mutation-testing/src/main/java/kopylove/dbf3/Calculator.java
@@ -0,0 +1,7 @@
+package kopylove.dbf3;
+
+public class Calculator {
+    public int add(int a, int b) { 
+        return a + b; 
+    } 
+}
