339d99b4aab8ca694c8ed2160ac82693148c26bb
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-11-17 16:23:05 +0300

    properties

--- spring-configuration-properties-record-class/src/test/java/c6f4/RecordClassPropertiesApplication.java
+++ spring-configuration-properties-record-class/src/test/java/c6f4/RecordClassPropertiesApplication.java
@@ -2,12 +2,11 @@ package c6f4;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 //@EnableConfigurationProperties(ParticularProperties.class) or ConfigurationPropertiesScan
 @ConfigurationPropertiesScan
-public class PropertiesApplication
+public class RecordClassPropertiesApplication
 {
 
 
